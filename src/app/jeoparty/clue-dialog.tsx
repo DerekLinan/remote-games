dc13bc66e4274607a99a3950b4bac775432c38a3
DerekLinan <4261674+DerekLinan@users.noreply.github.com>
2023-06-15 21:09:52 -0600

    feat(jeoparty): style clue-dialog

--- src/app/jeoparty/clue-dialog.tsx
+++ src/app/jeoparty/clue-dialog.tsx
@@ -1,4 +1,12 @@
-import { RefObject, forwardRef, useEffect, useRef } from 'react';
+import {
+  ButtonHTMLAttributes,
+  HtmlHTMLAttributes,
+  ReactNode,
+  RefObject,
+  forwardRef,
+  useEffect,
+  useRef,
+} from 'react';
 import { JeopardyClue } from './types';
 
 type Props = {
@@ -6,6 +14,23 @@ type Props = {
   clue?: JeopardyClue;
   setSquareState: (correct: boolean) => void;
 };
+
+function Button({
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: ReactNode;
+}) {
+  console.log(props.className);
+  return (
+    <button
+      className={`rounded bg-blue-300 px-2 ${props.className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
 
 export default function ClueDialog({ open, clue, setSquareState }: Props) {
   console.log(open);
@@ -26,14 +51,33 @@ export default function ClueDialog({ open, clue, setSquareState }: Props) {
   return (
     <dialog
       ref={dialogRef}
-      className='backdrop:bg-opacity-80 backdrop:backdrop-blur backdrop:bg-black'
+      className='backdrop:bg-opacity-80 backdrop:backdrop-blur backdrop:bg-black bg-gradient-to-tr from-blue-800 to-blue-700 lg:max-w-4xl'
       onCancel={() => setSquareState(false)}
     >
       {clue && (
         <>
-          <div>{new Date(clue.airdate).getFullYear()}</div>
-          <div>{clue.question}</div>
-          <div>{clue.value}</div>
+          <div className='mb-4 flex justify-between text-2xl lg:text-3xl text-center'>
+            <p>
+              Aired: <b>{new Date(clue.airdate).getFullYear()}</b>
+            </p>
+            <p>
+              Points: <b>{clue.value}</b>
+            </p>
+          </div>
+          <div className='my-4 font-bold text-4xl text-center lg:text-5xl select-none bg-clip-text text-transparent bg-gradient-to-br from-yellow-500 to-yellow-200'>
+            {clue.question}
+          </div>
+          <form className='text-xl m-4 flex flex-col gap-2 text-center'>
+            <p>Type your guess below:</p>
+            <input
+              type='text'
+              className='rounded px-2 py-1 text-2xl bg-blue-300 w-full text-center'
+            />
+            <div className='flex gap-4 justify-center'>
+              <Button onSubmit={() => {}}>Give Up</Button>
+              <Button onSubmit={() => {}}>Guess</Button>
+            </div>
+          </form>
         </>
       )}
     </dialog>
